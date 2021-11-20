229563e0b33a60478fb4eee5e70d35f1a0c214da
kleeenexfeu <mardinli.raphael@gmail.com>
2021-11-20 01:01:43 +0100

    100% catch to run through game

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -13616,7 +13616,7 @@ static void Cmd_handleballthrow(void)
         }
         #if B_BALL_CATCH_CANT_FAIL != TRUE
         if (odds > 254) // mon caught
-		#endif
+        #endif
         {
             BtlController_EmitBallThrowAnim(0, BALL_3_SHAKES_SUCCESS);
             MarkBattlerForControllerExec(gActiveBattler);
