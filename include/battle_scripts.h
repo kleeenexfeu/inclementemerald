5614a24b0e4b38f4c5f7c357c69e6c0996ccb477
kleeenexfeu <mardinli.raphael@gmail.com>
2021-12-03 01:28:54 +0100

    declare bs [skip ci]

--- include/battle_scripts.h
+++ include/battle_scripts.h
@@ -139,6 +139,7 @@ extern const u8 BattleScript_MoveEffectPayDay[];
 extern const u8 BattleScript_MoveEffectWrap[];
 extern const u8 BattleScript_MoveEffectConfusion[];
 extern const u8 BattleScript_MoveEffectRecoil[];
+extern const u8 BattleScript_DoRecoil[];
 extern const u8 BattleScript_DoRecoil33[];
 extern const u8 BattleScript_Recoil33End[];
 extern const u8 BattleScript_ItemSteal[];
