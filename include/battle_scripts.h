591e82c399eca2617247b217523656ef59d46ba3
kleeenexfeu <94004034+kleeenexfeu@users.noreply.github.com>
2023-03-27 15:47:02 +0200

    powethief

--- include/battle_scripts.h
+++ include/battle_scripts.h
@@ -316,6 +316,7 @@ extern const u8 BattleScript_ElectricSurgeActivates[];
 extern const u8 BattleScript_SpectralThiefSteal[];
 extern const u8 BattleScript_StatUpMsg[];
 extern const u8 BattleScript_DefiantActivates[];
+extern const u8 BattleScript_PowerThiefActivates[];
 extern const u8 BattleScript_PowderMoveNoEffect[];
 extern const u8 BattleScript_GrassyTerrainHeals[];
 extern const u8 BattleScript_VCreateStatLoss[];
